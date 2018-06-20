daf87d302ef9a75db40a00596b75442bb851e0bd
MadhuraBuchake <34618545+MadhuraBuchake@users.noreply.github.com>
2018-06-20 19:38:29 -0400

    Update Mispriced Diamonds.r

--- Mispriced Diamonds.r
+++ Mispriced Diamonds.r
@@ -3,6 +3,6 @@ ggplot(data=mydata[mydata$carat<2.5,],
        aes(x=carat, y=price, colour=clarity)) +
   geom_point(alpha=0.1) + geom_smooth()
 
-![mispriced diamonds plot](https://user-images.githubusercontent.com/34618545/41689957-522b0562-74c1-11e8-9f35-78664f4e9917.png)
+https://user-images.githubusercontent.com/34618545/41689957-522b0562-74c1-11e8-9f35-78664f4e9917.png
 
 
