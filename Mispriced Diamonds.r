427c0b335c98df84e2656d5524312ecbcc2c0460
MadhuraBuchake <34618545+MadhuraBuchake@users.noreply.github.com>
2018-06-20 20:48:14 -0400

    Update Mispriced Diamonds.r

--- Mispriced Diamonds.r
+++ Mispriced Diamonds.r
@@ -3,6 +3,11 @@ ggplot(data=mydata[mydata$carat<2.5,],
        aes(x=carat, y=price, colour=clarity)) +
   geom_point(alpha=0.1) + geom_smooth()
 
-https://user-images.githubusercontent.com/34618545/41689957-522b0562-74c1-11e8-9f35-78664f4e9917.png
+#To view the plot, please check the Read Me file.
+#Data Insights: 
+#From the plot we can observe that clarity of 'IF' type of diamond is the best of all and clarity of 'I1' is the least of all.
+#Since there is some intersection of lines for price range between 15000 and 175000 AND around 2.0 carat of diamonds, diamonds are mispriced for this category
+#Wherever there is intersection of lines, we can say diamonds are mispriced. 
+#We can also see that there is higher probability of purchasing 1carat diamonds for price between 4000 and 6000.
 
 
